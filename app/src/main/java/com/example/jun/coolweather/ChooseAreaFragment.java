63f045012004ccb2325440ea23a686e3008dca8f
Jun <Zhujun7818@gmail.com>
2018-07-08 21:59:30 +0800

    debug
    province
    city
    country

--- app/src/main/java/com/example/jun/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/jun/coolweather/ChooseAreaFragment.java
@@ -13,17 +13,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jun.coolweather.db.City;
 import com.example.jun.coolweather.db.Country;
 import com.example.jun.coolweather.db.Province;
+import com.example.jun.coolweather.util.HttpUtil;
+import com.example.jun.coolweather.util.Utility;
+import com.google.gson.annotations.Until;
 
 import org.litepal.LitePal;
 import org.litepal.crud.DataSupport;
 import org.litepal.crud.LitePalSupport;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ChooseAreaFragment extends Fragment {
     public static final int LEVEL_PROVINCE=0;
@@ -69,6 +78,9 @@ public class ChooseAreaFragment extends Fragment {
                 if (currentLevel==LEVEL_PROVINCE){
                     selectedProvince=provinceList.get(position);
                     queryCities();
+                }else if (currentLevel==LEVEL_CITY){
+                    selectedCity=cityList.get(position);
+                    queryCounties();
                 }
             }
         });
@@ -85,6 +97,65 @@ public class ChooseAreaFragment extends Fragment {
         queryProvinces();
     }
 
+
+    private void queryFromServer(String address, final String type) {
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText=response.body().string();
+                boolean result =false;
+                if ("province".equals(type)){
+                    result= Utility.handleProvinceResponse(responseText);
+                }else if ("city".equals(type)){
+                    result= Utility.handleCityResponse(responseText,selectedProvince.getId());
+                }else if ("country".equals(type)){
+                    result=Utility.handleCountryResponse(responseText,selectedCity.getId());
+                }
+                if (result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)){
+                                queryProvinces();;
+                            }else if ("city".equals(type)){
+                                queryCities();
+                            }else if ("country".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    private void showProgressDialog(){
+        if (progressDialog==null){
+            progressDialog=new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+    private void closeProgressDialog(){
+        progressDialog.dismiss();
+    }
+
     private void queryProvinces() {
         titleText.setText("中国");
         backButton.setVisibility(View.GONE);
@@ -92,7 +163,7 @@ public class ChooseAreaFragment extends Fragment {
         if (provinceList.size()>0){
             dataList.clear();
             for (Province province:provinceList
-                 ) {
+                    ) {
                 dataList.add(province.getProvinceName());
 
             }
@@ -105,9 +176,6 @@ public class ChooseAreaFragment extends Fragment {
             queryFromServer(address,"province");
         }
 
-    }
-
-    private void queryFromServer(String address, String province) {
     }
 
     private void queryCities() {
@@ -128,6 +196,28 @@ public class ChooseAreaFragment extends Fragment {
             int provinceCode=selectedProvince.getProvinceCode();
             String address="http://guolin.tech/api/china/"+provinceCode;
             queryFromServer(address,"city");
+        }
+    }
+
+    private void queryCounties() {
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countryList=DataSupport.where("cityid=?",String.valueOf(selectedCity.getId())).find(Country.class);
+        if (countryList.size()>0){
+            dataList.clear();
+            for (Country country:countryList
+                    ) {
+                dataList.add(country.getCountryName());
+
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_COUNTRY;
+        }else {
+            int provinceCode=selectedProvince.getProvinceCode();
+            int cityCode=selectedCity.getCityCode();
+            String address="http://guolin.tech/api/china/"+provinceCode+"/"+cityCode;
+            queryFromServer(address,"country");
         }
     }
 }
--- app/src/main/java/com/example/jun/coolweather/util/Utility.java
+++ app/src/main/java/com/example/jun/coolweather/util/Utility.java
@@ -15,7 +15,7 @@ public class Utility {
         if (!TextUtils.isEmpty(response)){
             try{
                 JSONArray allProvinces=new JSONArray(response);
-                for (int i=0;i>allProvinces.length();i++){
+                for (int i=0;i<allProvinces.length();i++){
                     JSONObject provinceObject=allProvinces.getJSONObject(i);
                     Province province=new Province();
                     province.setProvinceName(provinceObject.getString("name"));
@@ -35,12 +35,13 @@ public class Utility {
         if (!TextUtils.isEmpty(response)){
             try{
                 JSONArray allCities=new JSONArray(response);
-                for (int i=0;i>allCities.length();i++){
+                for (int i=0;i<allCities.length();i++){
                     JSONObject cityObject=allCities.getJSONObject(i);
                     City city=new City();
                     city.setCityName(cityObject.getString("name"));
                     city.setCityCode(cityObject.getInt("id"));
                     city.setProvinceId(provinceId);
+                    city.save();
                 }
                 return true;
             }
@@ -55,11 +56,11 @@ public class Utility {
         if (!TextUtils.isEmpty(response)){
             try{
                 JSONArray allCountries=new JSONArray(response);
-                for (int i=0;i>allCountries.length();i++){
+                for (int i=0;i<allCountries.length();i++){
                     JSONObject countryObject=allCountries.getJSONObject(i);
                     Country country=new Country();
                     country.setCountryName(countryObject.getString("name"));
-                    country.setWeatherId(countryObject.getInt("weather_id"));
+                    country.setWeatherId(countryObject.getString("weather_id"));
                     country.setCityId(cityId);
                     country.save();
                 }
